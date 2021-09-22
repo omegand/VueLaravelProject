23492f00a993476b171301c9876f192e6b9f3138
Šarūnas <sarunyss@gmail.com>
2021-09-22 16:01:37 +0300

    seed

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Post::create(
+            [
+                'body' => 'Nesiseka man išviso',
+                'title' => 'Bandymas1',
+            ]
+        );
+        Post::create(
+            [
+                'body' => 'O gal sekasi?',
+                'title' => 'Bandymas2',
+            ]
+        );
+    }
+}
