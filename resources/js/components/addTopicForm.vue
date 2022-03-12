041b1c6c60e0cc9093f863a3ccc274d384686852
Šarūnas <sarunyss@gmail.com>
2022-03-12 13:50:12 +0200

    refactor

--- resources/js/components/addTopicForm.vue
+++ resources/js/components/addTopicForm.vue
@@ -1,89 +1,93 @@
 <template>
-  <div>
-    <div v-if="this.$user.admin" class="row" style="background: #1f1d36">
-      <div class="col-4 offset-4 mt-5 mb-5">
-        <h3 style="color: #e9a6a6; text-align: center">Adding a Topic</h3>
-        <form @submit="onSubmit">
-          <div class="form-row">
-            <input
-              v-model="topic.title"
-              name="title"
-              class="form-control"
-              placeholder="Title..."
-            />
-          </div>
-          <div class="form-row">
-            <input
-              v-model="topic.desc"
-              name="desc"
-              class="form-control"
-              placeholder="Description..."
-            />
-          </div>
-          <div class="form-row">
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </div>
-        </form>
-      </div>
-    </div>
-    <div v-else class="text">You do not have access to this page.</div>
-  </div>
+	<div>
+		<div v-if="this.$user.admin" class="row" style="background: #1f1d36">
+			<div class="col-4 offset-4 mt-5 mb-5">
+				<h3 style="color: #e9a6a6; text-align: center">
+					Adding a Topic
+				</h3>
+				<form @submit="onSubmit">
+					<div class="form-row">
+						<input
+							v-model="topic.title"
+							name="title"
+							class="form-control"
+							placeholder="Title..."
+						/>
+					</div>
+					<div class="form-row">
+						<input
+							v-model="topic.desc"
+							name="desc"
+							class="form-control"
+							placeholder="Description..."
+						/>
+					</div>
+					<div class="form-row">
+						<button type="submit" class="btn btn-primary">
+							Submit
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+		<div v-else class="text">You do not have access to this page.</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "addTopic",
-  data: function () {
-    return {
-      topic: {
-        title: "",
-        desc: "",
-      },
-    };
-  },
-  methods: {
-    onSubmit(e) {
-      e.preventDefault();
-      if (this.topic.title == "") {
-        alert("Title is empty.");
-        return;
-      }
-      if (this.topic.desc == "") {
-        alert("Description is empty.");
-        return;
-      }
-      axios
-        .post("api/topic", {
-          title: this.topic.title,
-          desc: this.topic.desc,
-        })
-        .then((response) => {
-          if (response.status == 201) {
-            this.topic.title = "";
-            this.topic.desc = "";
-            this.$forceUpdate();
-          }
-        })
-        .catch((error) => {
-          alert("Topic already exists.");
-        });
-    },
-  },
+	name: "addTopic",
+	data: function () {
+		return {
+			topic: {
+				title: "",
+				desc: "",
+			},
+		};
+	},
+	methods: {
+		onSubmit(e) {
+			e.preventDefault();
+			if (this.topic.title == "") {
+				alert("Title is empty.");
+				return;
+			}
+			if (this.topic.desc == "") {
+				alert("Description is empty.");
+				return;
+			}
+			axios
+				.post("api/topic", {
+					title: this.topic.title,
+					desc: this.topic.desc,
+				})
+				.then((response) => {
+					if (response.status == 201) {
+						this.topic.title = "";
+						this.topic.desc = "";
+						this.$forceUpdate();
+					}
+				})
+				.catch((error) => {
+					alert("Topic already exists.");
+				});
+		},
+	},
 };
 </script>
 <style scoped>
 .row {
-  text-align: center;
+	text-align: center;
 }
 .form-row {
-  margin-bottom: 12px;
-  text-align: center;
+	margin-bottom: 12px;
+	text-align: center;
 }
 .text {
-  color: #ffc2b4;
-  background: #1f1d36;
-  text-align: center;
-  font-size: 3.6em;
-  padding: 6em;
+	color: #ffc2b4;
+	background: #1f1d36;
+	text-align: center;
+	font-size: 3.6em;
+	padding: 6em;
 }
 </style>
--- resources/js/components/footer.vue
+++ resources/js/components/footer.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="footer">
-    Copyright &copy; 2021
-    <br />
-    <router-link style="text-decoration: none; color: inherit" to="/about">
-      <h5>About</h5>
-    </router-link>
-  </div>
+	<div class="footer">
+		Copyright &copy; 2021
+		<br />
+		<router-link style="text-decoration: none; color: inherit" to="/about">
+			<h5>About</h5>
+		</router-link>
+	</div>
 </template>
 
 <script>
@@ -14,10 +14,9 @@ export default {};
 
 <style scoped>
 .footer {
-  padding-top: 20px;
-  text-align: center;
-  color: lightgreen;
-  background: #3f3351;
-  box-shadow: 0 50vh 0 50vh #3f3351;
+	text-align: center;
+	color: lightgreen;
+	background: #3f3351;
+	box-shadow: 0 50vh 0 50vh #3f3351;
 }
 </style>
